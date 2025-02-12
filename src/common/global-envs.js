f24d1e1d7a51603963f59c03ac6da854b8312709
Vitali Tsishkevich <vtsishke@its.jnj.com>
2025-02-12 11:33:42 +0300

    update env file name

--- src/common/global-envs.js
+++ src/common/global-envs.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const dotenvPath = path.resolve(__dirname, "../../.env.local");
+const dotenvPath = path.resolve(__dirname, "../../.env");
 dotenv.config({ path: dotenvPath });
 
 export const TOKEN = process.env.TOKEN;
