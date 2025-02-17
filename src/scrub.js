c3809b4ed01e3f097e0630021fc55af2a5b4624c
Vitali Tsishkevich <vtsishke@its.jnj.com>
2025-02-17 21:45:18 +0300

    add no sandbox

--- src/scrub.js
+++ src/scrub.js
@@ -40,7 +40,9 @@ export const scrub = async (init = false) => {
 
   const bot = new Bot(TOKEN);
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   // await page.setViewport({
   //   width: 1366,
